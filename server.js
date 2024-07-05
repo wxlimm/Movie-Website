0aa5e052946e51891bc0ba3e6fa72f600fbcefcc
wxlimm <wxlim2003@gmail.com>
2024-07-06 00:24:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-// DISM FT 2A 02
-// LIM WEI XIONG
-// P2026198
-
 var app=require('./app.js');
 var port=8081;
 
 var server=app.listen(port,function(){
-
     console.log("App hosted at localhost:"+port);
-
-    
 });
